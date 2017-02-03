4d7e759519ea1474b5e7d446503fba3d00103511
Vasyl Melnychuk <vasyl.melnychuk@4finance.com>
2017-02-03 16:42:19 +0100

    fix typo in word

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ clone-repo()
 {
     echo "Cloning github repo..."
     git clone https://github.com/vmelnychuk/vax.git --origin upstream --recursive
-    echo "Cleaning up: romove bootstrap.sh"
+    echo "Cleaning up: remove bootstrap.sh"
     rm bootstrap.sh
     echo "Run the following commands to build vax:"
     echo "cd vax"
